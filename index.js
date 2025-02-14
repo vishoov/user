738a76f49d7e6cd147c4fb88b8e69d29656963bb
Vishoo <vverma971@gmail.com>
2025-02-14 22:25:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+
+
+//RESTful API - Representational State Transfer
+//Whatever information is required to complete a request
+//is contained in the request itself. 
+
+//updating password in profile-> conflict -> profile fetching then updating password
+                                                // get request      //put 
+
+//Highly Scalable   -> RESTful API architecture 
+//Highly Maintainable -> MVC Structure
+//Highly Reliable -> Error Handling
+//Highly Secure -> Authentication and Authorization
+
+
+
+
+
+//user management api
+
+//Home page
+
+
+//Create Account + Authentication
+
+
+
+//Login + Authentication
+
+
+
+
+
+//Profile
+
+
+
+//Posts
+
+
+
+
+//Authentication is the process of verifying the identity of a user.
+
+
+
+//Logout
+
+
+
+
+//Change Password
+
+
+
+
+//Delete Account
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+})
